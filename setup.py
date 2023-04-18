ae195f950871f001c03a1851df3bdb831b4d5acd
Wen Kokke <wenkokke@users.noreply.github.com>
2023-04-18 19:22:03 +0100

    Use find_libpython

--- setup.py
+++ setup.py
@@ -10,6 +10,7 @@ from distutils.errors import (
     DistutilsSetupError,
 )
 from distutils.spawn import find_executable
+from find_libpython import find_libpython
 
 ext_modules = [
     Extension(
@@ -24,7 +25,11 @@ class cabal_build_ext(build_ext):
         super().finalize_options()
 
         if sys.platform in ["win32", "cygwin"]:
-            self.libraries.append("python%d%d" % sys.version_info[:2])
+            library_dir, library = os.path.split(find_libpython())
+            libname, _libext = os.path.splitext(library)
+            libname = libname[3:] if libname.startswith("lib") else libname
+            self.libraries.append(libname)
+            self.library_dirs.append(library_dir)
 
     def build_extension(self, ext: Extension):
         # Taken from setuptools:
