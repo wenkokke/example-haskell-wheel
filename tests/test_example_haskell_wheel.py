24861e424f3ae26a0b85edf188377237a685ae8d
Wen Kokke <wenkokke@users.noreply.github.com>
2023-05-19 21:41:12 +0100

    Fix name

--- tests/test_example_haskell_wheel.py
+++ tests/test_example_haskell_wheel.py
@@ -7,8 +7,8 @@ def test_example_haskell_wheel_main() -> None:
     import example_haskell_wheel
     import subprocess
     
-    def test_fib(input: int, result: int) -> None:
+    def test_case(input: int, result: int) -> None:
         output = subprocess.check_output(["example-haskell-wheel", f"{input}"]).decode("utf-8").strip()
         assert output == f"fib {input} -> {result}"
 
-    test_fin(11, 89)
+    test_case(11, 89)
