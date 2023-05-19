b1ab287e6a2a8620f30ea2f50741282930a22f43
Wen Kokke <wenkokke@users.noreply.github.com>
2023-05-19 21:32:18 +0100

    Add back executables test

--- tests/test_example_haskell_wheel.py
+++ tests/test_example_haskell_wheel.py
@@ -2,3 +2,10 @@ def test_example_haskell_wheel_version() -> None:
     import example_haskell_wheel
 
     assert example_haskell_wheel.version() == example_haskell_wheel.VERSION
+
+def test_example_haskell_wheel_main() -> None:
+    import example_haskell_wheel
+    import subprocess
+    
+    output = subprocess.check_output(["example-haskell-wheel", "11"]).strip()
+    assert output == "fib 11 -> 89"
