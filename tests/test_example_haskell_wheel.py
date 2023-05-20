de09d223330c93afd011cd14892c913f6f86960e
Wen Kokke <wenkokke@users.noreply.github.com>
2023-05-20 19:48:38 +0100

    Restructure tests

--- tests/test_example_haskell_wheel.py
+++ tests/test_example_haskell_wheel.py
@@ -1,14 +1,26 @@
+from typing import List
+
+
 def test_example_haskell_wheel_version() -> None:
     import example_haskell_wheel
 
     assert example_haskell_wheel.version() == example_haskell_wheel.VERSION
 
-def test_example_haskell_wheel_main() -> None:
-    import example_haskell_wheel
-    import subprocess
-    
-    def test_case(input: int, result: int) -> None:
-        output = subprocess.check_output(["example-haskell-wheel", f"{input}"]).decode("utf-8").strip()
-        assert output == f"fib {input} -> {result}"
 
-    test_case(11, 89)
+def assert_example_haskell_wheel_main(args: List[str], golden_output: str) -> None:
+    import subprocess
+
+    actual_output = (
+        subprocess.check_output(["example-haskell-wheel", *args])
+        .decode("utf-8")
+        .strip()
+    )
+    assert actual_output == golden_output
+
+
+def test_example_haskell_wheel_main() -> None:
+    assert_example_haskell_wheel_main(["11"], "fib 11 -> 89")
+    assert_example_haskell_wheel_main(["23"], "fib 23 -> 28657")
+    assert_example_haskell_wheel_main(["35"], "fib 35 -> 9227465")
+    assert_example_haskell_wheel_main(["47"], "fib 47 -> 2971215073")
+    assert_example_haskell_wheel_main(["59"], "fib 59 -> 956722026041")
