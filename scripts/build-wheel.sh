5cf81e30e6c02635312c21c74bb26b27bb5448a2
Wen Kokke <wenkokke@users.noreply.github.com>
2023-10-31 16:09:07 +0000

    Update cabal before build

--- scripts/build-wheel.sh
+++ scripts/build-wheel.sh
@@ -11,57 +11,69 @@ platform=$("${env_python}" -c 'import sys; print(sys.platform)')
 
 # Check if dependencies are available:
 
+# Check if cabal is available:
+env_cabal="$(which cabal)"
+if [ "${env_cabal}" = '' ]
+then
+    echo "${0} requires 'cabal'"
+    echo "See: https://cabal.readthedocs.io/en/stable/"
+    exit 1
+fi
+
 # Check if build is available:
 has_build=$("${env_python}" -c 'import importlib.util; print(importlib.util.find_spec("build") is not None)')
 if [ "${has_build}" = 'False' ]
 then
-  echo "${0} requires 'build'"
-  echo "See: https://pypi.org/project/build/"
-  exit 1
+    echo "${0} requires 'build'"
+    echo "See: https://pypi.org/project/build/"
+    exit 1
 fi
 
 # Check if platform-specific dependencies are available:
 case "${platform}" in
-  'linux')
-    # Check if auditwheel is available:
-    has_auditwheel=$("${env_python}" -c 'import importlib.util; print(importlib.util.find_spec("auditwheel") is not None)')
-    if [ "${has_auditwheel}" = 'False' ]
-    then
-      echo "${0} requires 'auditwheel'"
-      echo "See: https://pypi.org/project/auditwheel/"
-      exit 1
-    fi
+    'linux')
+        # Check if auditwheel is available:
+        has_auditwheel=$("${env_python}" -c 'import importlib.util; print(importlib.util.find_spec("auditwheel") is not None)')
+        if [ "${has_auditwheel}" = 'False' ]
+        then
+            echo "${0} requires 'auditwheel'"
+            echo "See: https://pypi.org/project/auditwheel/"
+            exit 1
+        fi
     ;;
-  'darwin')
-    # Check if delocate is available:
-    has_delocate=$("${env_python}" -c 'import importlib.util; print(importlib.util.find_spec("delocate") is not None)')
-    if [ "${has_delocate}" = 'False' ]
-    then
-      echo "${0} requires 'delocate'"
-      echo "See: https://pypi.org/project/delocate/"
-      exit 1
-    fi
+    'darwin')
+        # Check if delocate is available:
+        has_delocate=$("${env_python}" -c 'import importlib.util; print(importlib.util.find_spec("delocate") is not None)')
+        if [ "${has_delocate}" = 'False' ]
+        then
+            echo "${0} requires 'delocate'"
+            echo "See: https://pypi.org/project/delocate/"
+            exit 1
+        fi
     ;;
 esac
+
+# Update the hackage package list
+"${env_cabal}" v2-update
 
 # Build the wheel
 "${env_python}" -m build --wheel --outdir "${dist_tmp_dir}" "${package_root}"
 
 # Delocate the wheel
 case "${platform}" in
-  'linux')
-    # Repair wheel with auditwheel
-    libc_xy="$("${env_python}" -c 'import platform; print(platform.libc_ver()[1].replace(".","_"))')"
-    machine="$("${env_python}" -c 'import platform; print(platform.machine())')"
-    auditwheel repair --wheel-dir "${dist_dir}" --plat "manylinux_${libc_xy}_${machine}" "${dist_tmp_dir}"/*.whl
+    'linux')
+        # Repair wheel with auditwheel
+        libc_xy="$("${env_python}" -c 'import platform; print(platform.libc_ver()[1].replace(".","_"))')"
+        machine="$("${env_python}" -c 'import platform; print(platform.machine())')"
+        auditwheel repair --wheel-dir "${dist_dir}" --plat "manylinux_${libc_xy}_${machine}" "${dist_tmp_dir}"/*.whl
     ;;
-  'darwin')
-    # Repair wheel with delocate-wheel
-    delocate-wheel --wheel-dir "${dist_dir}" "${dist_tmp_dir}"/*.whl
+    'darwin')
+        # Repair wheel with delocate-wheel
+        delocate-wheel --wheel-dir "${dist_dir}" "${dist_tmp_dir}"/*.whl
     ;;
-  *)
-    mkdir -p "${dist_dir}"
-    cp "${dist_tmp_dir}"/*.whl "${dist_dir}"/
+    *)
+        mkdir -p "${dist_dir}"
+        cp "${dist_tmp_dir}"/*.whl "${dist_dir}"/
     ;;
 esac
 echo "The built wheel is located in ${dist_dir}"
