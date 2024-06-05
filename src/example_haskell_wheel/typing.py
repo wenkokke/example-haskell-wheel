fd454c1c7c11ca91272392735c477dd4bc740315
Wen Kokke <wenkokke@users.noreply.github.com>
2024-06-05 12:51:18 +0100

    Use typing_extensions for Protocol for Python <3.8

--- src/example_haskell_wheel/typing.py
+++ src/example_haskell_wheel/typing.py
@@ -1,4 +1,11 @@
-from typing import List, Protocol
+import sys
+
+from typing import List
+
+if sys.version_info < (3, 8):
+    from typing_extensions import Protocol
+else:
+    from typing import Protocol
 
 
 class Session(Protocol):
